79c79b7095cd81adb224a23ed1d79a4b2175eba3
Andrew Sun <98356114+sunandrew03@users.noreply.github.com>
2022-12-13 21:27:56 -0500

    Add files via upload

---
+++ flask-app/src/__init__.py
@@ -0,0 +1,28 @@
+# Some set up for the application 
+
+from flask import Flask
+from flaskext.mysql import MySQL
+
+# create a MySQL object that we will use in other parts of the API
+db = MySQL()
+
+def create_app():
+    app = Flask(__name__)
+    
+    # secret key that will be used for securely signing the session 
+    # cookie and can be used for any other security related needs by 
+    # extensions or your application
+    app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
+
+    # these are for the DB object to be able to connect to MySQL. 
+    app.config['MYSQL_DATABASE_USER'] = 'webapp'
+    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
+    app.config['MYSQL_DATABASE_HOST'] = 'db'
+    app.config['MYSQL_DATABASE_PORT'] = 3306
+    app.config['MYSQL_DATABASE_DB'] = 'classicmodels'  # Change this to your DB name
+
+    # Initialize the database object with the settings above. 
+    db.init_app(app)
+
+
+    return app
